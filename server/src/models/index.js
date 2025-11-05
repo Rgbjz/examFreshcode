d56b6d9dd98b2016e9761c56733092e14cd68957
Lukashev Oles <lukasevoles@gmail.com>
2025-11-05 02:01:36 +0200

    refactor: move DB credentials from postgresConfig.json to .env for security

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,31 +2,29 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
 
-// Визначаємо шлях до конфігураційного файлу через змінні середовища
-const configPath =
-    process.env.PG_CONFIG_PATH ||
-    path.join(__dirname, '..', 'config', 'postgresConfig.json');
-const config = require(configPath)[env];
+require('dotenv').config();
+
 const db = {};
 
 const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+    }
 );
 
-// Зчитуємо всі файли моделей та додаємо їх до db
 fs.readdirSync(__dirname)
-    .filter(file => {
-        return (
+    .filter(
+        file =>
             file.indexOf('.') !== 0 &&
             file !== basename &&
             file.slice(-3) === '.js'
-        );
-    })
+    )
     .forEach(file => {
         const model = require(path.join(__dirname, file))(
             sequelize,
@@ -35,11 +33,8 @@ fs.readdirSync(__dirname)
         db[model.name] = model;
     });
 
-// Визначаємо асоціації для моделей
 Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+    if (db[modelName].associate) db[modelName].associate(db);
 });
 
 db.sequelize = sequelize;
